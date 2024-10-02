ce95dd7dcf7fe5c5858ffd58f1b305838076047c
WillHDev <will.v.holcombe@gmail.com>
2024-10-02 14:22:21 -0500

    fixtures working

--- tests/deposit.spec.ts
+++ tests/deposit.spec.ts
@@ -1,40 +1,16 @@
-import { test, expect } from '@playwright/test';
-import { HomePage } from './pages/home';
-import { LoginPage } from './pages/auth';
+import { test, expect } from './fixtures/xyz';
 
-test('Make a deposit', async ({ page }) => {
+test('Make a deposit', async ({ homePage, loginPage, page }) => {
 
-  const homePage = new HomePage(page);
-  const loginPage = new LoginPage(page);
-  
-  // await page.goto('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
-
-  // await page.getByText('Customer Login').click();
-  
-  // await page.locator('select.form-control').selectOption('Ron Weasly');
-    
-  // await page.getByText('Login').click();
-
-  // await expect(page.locator('.fontBig.ng-binding')).toHaveText('Ron Weasly');
-
-await loginPage.gotoUrl();
-await loginPage.login('Ron Weasly');
-
+  await loginPage.gotoUrl();
+  await loginPage.login('Ron Weasly');
   await homePage.deposit.click();
-
   await page.getByPlaceholder('amount').fill('750');
-
   await page.locator('form').locator("button[type=submit]").click();
-
   await expect(page.getByText('Deposit Successful')).toBeVisible();
-
   await page.waitForTimeout(5000);
-
   await homePage.transactions.click();
-
   const cellCount = await page.locator('td').count();
-
   await expect(page.locator('td').nth(Number(cellCount) - 2)).toHaveText('750');
-
 });
 
